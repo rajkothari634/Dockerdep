440a550e1b8f89240a414044d0caa0ef1a6a1972
Raj kothari <rkmeansrk39@gmail.com>
2020-04-28 18:45:16 +0530

    server file

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const tour = require("./tourController");
+
+// Constants
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
+// App
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/addTour", tour.createTour);
+
+dotenv.config({ path: "./config.env" });
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("DB successful"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/hitodockerdep", (req, res) => {
+  console.log(req.url);
+  res.status(200).json({
+    status: "200",
+    message: "hiconnectdep",
+  });
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}` + " changed data");
